4fce25ced2e3ccc15d4b731b77379f0ce9974a59
liushuangxi <729021170@163.com>
2017-04-16 23:25:00 +0800

    修改执行文件

--- src/Html/HtmlToPdf.php
+++ src/Html/HtmlToPdf.php
@@ -1,10 +1,41 @@
 <?php
-namespace Html;
+namespace Liushuangxi\Html;
 
+use Html\Tool;
+
+/**
+ * Class HtmlToPdf
+ * @package Html
+ */
 class HtmlToPdf
 {
-    public static function demo()
+    /**
+     * @param $list
+     * @param $destination
+     * @param string $options
+     * @return string
+     */
+    public static function generate($list, $destination, $options = '')
     {
-        echo time();
+        //脚本文件
+        $scriptPath = dirname(__DIR__) . "/Html/Tools/wkhtmltopdf";
+
+        //检查file/url
+        foreach ($list as $index => $file) {
+            if (!Tool::checkFile($file) && !Tool::checkUrl($file)) {
+                unset($list[$index]);
+            }
+        }
+
+        if (empty($list)) {
+            return '';
+        }
+
+        //文件列表
+        $list = @implode(' ', array_map(function ($url) {
+            return "'$url'";
+        }, $list));
+
+        return Tool::generateFile($scriptPath, $list, $destination, $options);
     }
 }
---
+++ src/Html/Tool.php
@@ -0,0 +1,79 @@
+<?php
+namespace Liushuangxi\Html;
+
+/**
+ * Class Tool
+ * @package Html
+ */
+class Tool
+{
+    /**
+     * 生成文件
+     *
+     * @param $scriptPath 脚本路径
+     * @param $list file/url列表
+     * @param $destination 　存放路径
+     * @param string $options 配置选项,字符串类型
+     *
+     * @return string
+     */
+    public static function generateFile($scriptPath, $list, $destination, $options = '')
+    {
+        $cmd = $scriptPath . " $options $list " . $destination;
+
+        $array = [
+            ["pipe", "r"],
+            ["pipe", "w"],
+            ["pipe", "w"]
+        ];
+
+        try {
+            $fp = @proc_open($cmd, $array, $pipes);
+            @proc_close($fp);
+        } catch (\Exception $e) {
+            echo $e->getMessage() . "\n";
+
+            return '';
+        }
+
+        if (file_exists($destination) && filesize($destination) > 1024) {
+            return $destination;
+        } else {
+            return '';
+        }
+    }
+
+    /**
+     * 检查文件有效性
+     *
+     * @param $file 文件绝对路径
+     *
+     * @return bool
+     */
+    public static function checkFile($file)
+    {
+        if (!file_exists($file) || !is_readable($file)) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    /**
+     * 检查链接有效性
+     *
+     * @param $url URL链接
+     *
+     * @return bool
+     */
+    public static function checkUrl($url)
+    {
+        $data = @get_headers($url);
+
+        if (empty($data) || !isset($data[0]) || !strpos($data[0], '200')) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+}
